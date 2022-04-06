f4b354b39429be5fd48e3cea0c08e206aae8bb6e
karinaghristova <karina.hristova@abv.bg>
2022-04-06 03:30:59 +0300

    Base controller added

---
+++ Plan2Day/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Plan2Day.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+
+    }
+}
--- Plan2Day/Controllers/HomeController.cs
+++ Plan2Day/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 
 namespace Plan2Day.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
