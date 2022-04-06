3b5fdbc9928d159389f5c78188b6cb3c7efe64ff
karinaghristova <karina.hristova@abv.bg>
2022-04-06 22:55:58 +0300

    Database models for movies and movie genres added

---
+++ Plan2Day.Infrastructure/Data/DbModels/Movies/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plan2Day.Infrastructure.Data.DbModels.Movies
+{
+    public class Movie
+    {
+        [Key]
+        public Guid Id { get; set; } = new Guid();
+
+        [Required]
+        [StringLength(100)]
+        public string ImdbOriginalId { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Title { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        public string? Year { get; set; }
+
+        [Required]
+        public int Runtime { get; set; }
+
+        [StringLength(10000)]
+        public string? Plot { get; set; }
+
+        public ICollection<MovieGenre> Genres { get; set; } = new List<MovieGenre>();
+    }
+}
---
+++ Plan2Day.Infrastructure/Data/DbModels/Movies/MovieGenre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plan2Day.Infrastructure.Data.DbModels.Movies
+{
+    public class MovieGenre
+    {
+        [Key]
+        public Guid Id { get; set; } = new Guid();
+
+        [Required]
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        public ICollection<Movie> Movies { get; set; } = new List<Movie>();
+
+    }
+}
