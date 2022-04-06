e4413248d536106385d8ea3ed03e1f8f938a9d8e
karinaghristova <karina.hristova@abv.bg>
2022-04-06 22:56:09 +0300

    Database updated

--- Plan2Day.Infrastructure/Data/ApplicationDbContext.cs
+++ Plan2Day.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Plan2Day.Infrastructure.Data.DbModels.Books;
+using Plan2Day.Infrastructure.Data.DbModels.Movies;
 
 namespace Plan2Day.Infrastructure.Data
 {
@@ -9,5 +11,12 @@ namespace Plan2Day.Infrastructure.Data
             : base(options)
         {
         }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookGenre> BookGenres { get; set; }
+
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<MovieGenre> MovieGenres { get; set; }
     }
 }
