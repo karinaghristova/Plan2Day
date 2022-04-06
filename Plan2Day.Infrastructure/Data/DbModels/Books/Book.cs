c84262d57da063c9549d1001a79ecff511fd9816
karinaghristova <karina.hristova@abv.bg>
2022-04-06 22:55:40 +0300

    Database models for books and book genres added

---
+++ Plan2Day.Infrastructure/Data/DbModels/Books/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plan2Day.Infrastructure.Data.DbModels.Books
+{
+    public class Book
+    {
+        [Key]
+        public Guid Id { get; set; } = new Guid();
+
+        [Required]
+        [StringLength(250)]
+        public string Title { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Author { get; set; }
+
+        [Required]
+        [StringLength(13)]
+        public string Isbn13 { get; set; }
+         
+        public int? Pages { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [StringLength(10000)]
+        public string? Description { get; set; }
+
+        public ICollection<BookGenre> Genres { get; set; } = new List<BookGenre>();
+    }
+}
---
+++ Plan2Day.Infrastructure/Data/DbModels/Books/BookGenre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plan2Day.Infrastructure.Data.DbModels.Books
+{
+    public class BookGenre
+    {
+        [Key]
+        public Guid Id { get; set; } = new Guid();
+
+        [Required]
+        [StringLength(250)]
+        public string Name { get; set; }
+
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
