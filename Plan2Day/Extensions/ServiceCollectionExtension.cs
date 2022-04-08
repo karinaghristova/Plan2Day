b7e0a98c34cacf589d2ab6a0ac88ae7d5f6e5990
karinaghristova <karina.hristova@abv.bg>
2022-04-09 00:52:20 +0300

    Service collection extenstion method added

---
+++ Plan2Day/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Plan2Day.Infrastructure.Data;
+using Plan2Day.Infrastructure.Data.Repositories;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicatioDbRepository, ApplicationDbRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContexts(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+    }
+}
--- Plan2Day/Program.cs
+++ Plan2Day/Program.cs
@@ -1,18 +1,12 @@
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Plan2Day.Core.Constants;
 using Plan2Day.Infrastructure.Data;
-using Plan2Day.Infrastructure.Data.Repositories;
 using Plan2Day.ModelBinders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
+builder.Services.AddApplicationDbContexts(builder.Configuration);
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
@@ -24,7 +18,7 @@ builder.Services.AddControllersWithViews()
         options.ModelBinderProviders.Insert(2, new DoubleModelBinderProvider());
     });
 
-builder.Services.AddScoped<IApplicatioDbRepository, ApplicationDbRepository>();
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
