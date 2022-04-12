a5607a661db4e7edb6d743088e3f69af9a29caa8
karinaghristova <karina.hristova@abv.bg>
2022-04-12 16:14:56 +0300

    Test project added

---
+++ Plan2DayTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Plan2DayTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
